a1bb883f96290450b140491dec0aa062b986dfa1
Patrick Mosby <info@schreiblogade.de>
2009-03-21 14:18:26 +0100

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DropAndMove
+//
+//  Created by Patrick on 21.03.09.
+//  Copyright Patrick Mosby 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
