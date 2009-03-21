1a23a0b1976c7b2275550b76c03ffcf1e7a93c48
Patrick Mosby <info@schreiblogade.de>
2009-03-21 15:09:08 +0100

    Implement performDragOperation method

--- DropView.m
+++ DropView.m
@@ -42,4 +42,52 @@
 - (BOOL)prepareForDragOperation:(id <NSDraggingInfo>)sender {
     return YES;
 }
+
+- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender {
+    NSPasteboard *paste = [sender draggingPasteboard];
+    NSArray *types = [NSArray arrayWithObjects:NSTIFFPboardType, NSFilenamesPboardType, nil];
+    NSString *desiredType = [paste availableTypeFromArray:types];
+    NSData *carriedData = [paste dataForType:desiredType];
+    
+    if (nil == carriedData) {
+        NSRunAlertPanel(@"Paste Error",
+                        @"Sorry, but the paste operation failed",
+                        nil,
+                        nil,
+                        nil);
+        return NO;
+    } else {
+        if ([desiredType isEqualToString:NSTIFFPboardType]) {
+            NSImage *newImage = [[NSImage alloc] initWithData:carriedData];
+            [self setImage:newImage];
+            [newImage release];
+        } else if ([desiredType isEqualToString:NSFilenamesPboardType]) {
+            NSArray *fileArray = [paste propertyListForType:@"NSFilenamesPboardType"];
+            NSString *path = [fileArray objectAtIndex:0];
+            NSImage *newImage = [[NSImage alloc] initWithContentsOfFile:path];
+            
+            if (nil == newImage) {
+                NSRunAlertPanel(@"File Reading Error",
+                                [NSString stringWithFormat:@"Sorry, but I failed to open the file at \"@\"", path],
+                                nil,
+                                nil,
+                                nil);
+                return NO;
+            } else {
+                [self setImage:newImage];
+            }
+            
+            [newImage release];
+        } else {
+            NSAssert(NO, @"This can't happen");
+            return NO;
+        }
+    }
+    
+    [self setNeedsDisplay:YES];
+    return YES;
+    
+    
+    
+}
 @end
