100a50947a209f240d71412261290919c45bdc84
Patrick Mosby <info@schreiblogade.de>
2009-03-21 14:36:34 +0100

    Add DropView (NSView subclass)

---
+++ DropView.h
@@ -0,0 +1,16 @@
+//
+//  DropView.h
+//  DropAndMove
+//
+//  Created by Patrick on 21.03.09.
+//  Copyright 2009 Patrick Mosby. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DropView : NSView {
+
+}
+
+@end
---
+++ DropView.m
@@ -0,0 +1,26 @@
+//
+//  DropView.m
+//  DropAndMove
+//
+//  Created by Patrick on 21.03.09.
+//  Copyright 2009 Patrick Mosby. All rights reserved.
+//
+
+#import "DropView.h"
+
+
+@implementation DropView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+    // Drawing code here.
+}
+
+@end
